20d8943db9710e50e54663b42e86c7d8f2b24f1d
Brendan Leibengood <15bleiben@gmail.com>
2024-01-30 15:44:31 -0500

    stuff

--- Lab3/src/Profile.py
+++ Lab3/src/Profile.py
@@ -3,24 +3,28 @@ import random
 
 prof={
         "alice": {
+            "image" : "people1.png",
             "name" : "Alice Smith", 
             "dob" : "Jan. 1",
             "email" : "alice@example.com"
         },
 
         "bob": {
+            "image" : "people2.png",
             "name" : "Bob Jones", 
             "dob" : "Dec. 31",
             "email" : "bob@bob.xyz"
         },
 
         "carol": {
+            "image" : "people3.png",
             "name" : "Carol Ling", 
             "dob" : "Jul. 17",
             "email" : "carol@example.com"
         },
 
         "dave": {
+            "image" : "people4.png",
             "name" : "Dave N. Port", 
             "dob" : "Mar. 14",
             "email" : "dave@dave.dave"
@@ -32,6 +36,6 @@ class Handler(tornado.web.RequestHandler):
         L = self.request.path.split("/")
         uname = L[2]
         info = prof[uname]
-        self.render("profilepage.html", 
+        self.render("profilepage.html", image=info["image"],
                     name=info["name"], dob=info["dob"],
                     email = info["email"])
